4665b30a07095504334b21607cc3c18c28ed4aac
carlo <carlo@DESKTOP-TLTLHAM>
2022-03-01 00:46:21 -0300

    Adicionar arquivos de projeto.

---
+++ AutomacaoWorkover/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomacaoWorkover")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomacaoWorkover")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95543801-189d-421f-aef4-03eba1e2716a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomacaoWorkover/RegistroDeCadastro.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace AutomacaoWorkover
+{
+
+    [TestClass]
+    public class RegistroDeCadastro
+    {
+        public IWebDriver Driver { get; set; }
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            ChromeOptions optionsChrome = new ChromeOptions();
+            optionsChrome.AddArgument("--start-maximized");
+            Driver = new ChromeDriver(optionsChrome);
+            Driver.Navigate().GoToUrl("https://alunos.workover.com.br/login");
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void RealizandoCadastroNoSite()
+        {
+            Driver.FindElement(By.ClassName("btn-secondary")).Click();
+        }
+    }
+}
