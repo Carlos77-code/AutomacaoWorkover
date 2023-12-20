e6ba9f08316092aadb1203b90991a534cb00c2c7
QAorDev <carlosalimafernandes@hotmail.com>
2023-12-20 00:13:18 -0300

    Criada a class "CadastroUser"
    Criada a classe cadastro de usuário, preciso encontrar um biblioteca para gerar massa fake.

---
+++ AutomacaoWorkover/Begin.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomacaoWorkover
+{
+    public class Begin
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void InicioTeste()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://workover.com.br/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void FimDoTeste()
+        {
+            
+        }
+    }
+}
---
+++ AutomacaoWorkover/CadastroUser.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace AutomacaoWorkover
+{
+    internal class CadastroUser : Begin
+    {
+        [Test]
+        public void Teste()
+        {
+            driver.FindElement(By.ClassName("btn-login")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//*[@id=\'loginModalElement\']/div/div/form/div[3]/div[2]/p/a/strong")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("name")).SendKeys("QAorDev");
+            driver.FindElement(By.Id("email")).SendKeys("qa@teste.com");
+            driver.FindElement(By.XPath("//*[@id=\'loginModalElement\']/div/div/form/div[3]/div[1]/button")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("password")).SendKeys("qa1234");
+            driver.FindElement(By.XPath("//*[@id=\'loginModalElement\']/div/div/form/div[3]/div[1]/button")).Click();
+        }
+    }
+}
