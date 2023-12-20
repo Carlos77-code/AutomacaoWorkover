ccca1ec20fd05f49a77a3951e5f2026399f708be
QAorDev <carlosalimafernandes@hotmail.com>
2023-12-19 21:37:57 -0300

    retorno ao projeto
    Criando estrutura correta para o projeto

--- AutomacaoWorkover/RegistroDeCadastro.cs
+++ AutomacaoWorkover/RegistroDeCadastro.cs
@@ -1,46 +1,28 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System;
-using Assert = NUnit.Framework.Assert;
 
 namespace AutomacaoWorkover
 {
-
-    [TestClass]
     public class RegistroDeCadastro
     {
-        public IWebDriver Driver { get; set; }
+        public IWebDriver Driver;
 
         [SetUp]
-        public void SetUpTest()
+        public void InicioTeste()
         {
-            ChromeOptions optionsChrome = new ChromeOptions();
-            optionsChrome.AddArgument("--start-maximized");
-            Driver = new ChromeDriver(optionsChrome);
-            Driver.Navigate().GoToUrl("https://alunos.workover.com.br/login");
-            System.Threading.Thread.Sleep(8000);
-        }
-
-        [TearDown]
-        public void TearDownTest()
-        {
-            Driver.Quit();
+            
         }
 
         [Test]
-        public void RealizandoCadastroNoSite()
+        public void Teste()
         {
-            Driver.FindElement(By.ClassName("btn-secondary")).Click();
-            Driver.FindElement(By.Id("name")).SendKeys("Fernando Medeiros");
-            Driver.FindElement(By.Id("email")).SendKeys("teste@gmail.com");
-            Driver.FindElement(By.Id("password")).SendKeys("12345678");
-            Driver.FindElement(By.Id("phone")).SendKeys("11984546145");
-            Driver.FindElement(By.ClassName("checkmark")).Click();
-            Driver.FindElement(By.XPath("/html/body/section/div[2]/form/div[4]/button")).Click();
 
-            Assert.AreEqual("Workover", Driver.Title);
+        }
+
+        [TearDown]
+        public void FimDoTeste()
+        {
+            
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Projeto em CSharp com Selenium, utilizando NUnit e .Net 5.0 (conferir)
+
+
+## Status do Projeto
+> Status do Projeto: Em desenvolvimento :warning:
+
+## Tarefas em aberto
+- [] Tarefa 1: Criar estrutura do projeto.
+- [x] Tarefa 2: Criar método para abrir/fechar o navegador e acessar o site Investidor 10.
+- [x] Tarefa 3: Criar as interações de clique e verificação da Home page.
+
+## Descrição do Projeto
+Este projeto tem por motivação realizar a automação de cadastro e e verificar se todas as telas e páginas de curso estão funcionando corretamente.
+
+
+
+## Linguagens e libs utilizadas :books:
+- .Net 
+    - Selenium
+
+## Rodando os testes
+
+Para rodar a automação, execute o seguinte comando
+
+```bash
+  F5 (irá rodar os testes em modo debug)
+  ctrl + F5 (irá rodar)
+  Test Explorer (nele dá para escolher se roda somente um teste ou todos que tiver)
+```
+
+### Resolvendo problemas
+-
+
+### Participante:
+
+|name|email|Cargo|
+| -------- | -------- | -------- 
+|Carlos L. Fernandes|carloslimafernandes@hotmail.com|QA|
